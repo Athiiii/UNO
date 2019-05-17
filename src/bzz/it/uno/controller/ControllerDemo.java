53a0a7c1e127149d8516aa7bea94761a4e860ea4
SeverinHersche <35527435+SeverinHersche@users.noreply.github.com>
2019-05-17 15:01:15 +0200

    Die Ordenerstruktur hatte Fehler, welche verbessert sind.

---
+++ src/bzz/it/uno/controller/ControllerDemo.java
@@ -0,0 +1,5 @@
+package bzz.it.uno.controller;
+//Diese Klasse wird nicht gebrauch. Sie ist nur da, um die Ordnerstruktur auf Github zu puschen
+public class ControllerDemo {
+
+}
--- src/bzz/it/uno/dao/DAODemo.java
+++ src/bzz/it/uno/dao/DAODemo.java
@@ -1,5 +1,5 @@
-package bzz.it.uno.dao.controller;
+package bzz.it.uno.dao;
 //Diese Klasse wird nicht gebrauch. Sie ist nur da, um die Ordnerstruktur auf Github zu puschen
-public class DemoController {
+public class DAODemo {
 
 }
