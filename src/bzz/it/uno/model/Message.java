0da3cd9d817038debcd67291c4f04f3edd05cb9c
SeverinHersche <35527435+SeverinHersche@users.noreply.github.com>
2019-06-21 15:35:14 +0200

    Added the entitty infront of model and edited the persistende xml

--- src/bzz/it/uno/model/Message.java
+++ src/bzz/it/uno/model/Message.java
@@ -1,5 +1,12 @@
 package bzz.it.uno.model;
 
-public class Message {
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "message")
+public class Message {
+	private int id;
+	private String text;
+	private User_Lobby userLobby;
 }
--- src/bzz/it/uno/model/User_Lobby.java
+++ src/bzz/it/uno/model/User_Lobby.java
@@ -1,11 +1,14 @@
 package bzz.it.uno.model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-
+import javax.persistence.Table;
+@Entity
+@Table(name = "user_Lobby")
 public class User_Lobby {
 	private int id;
 	private User user;
