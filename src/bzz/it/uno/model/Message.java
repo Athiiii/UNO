09baf8df8523654c994fac7cf789994de1868b9f
SeverinHersche <35527435+SeverinHersche@users.noreply.github.com>
2019-06-21 15:49:40 +0200

    Done the Moves and Message

--- src/bzz/it/uno/model/Message.java
+++ src/bzz/it/uno/model/Message.java
@@ -1,6 +1,12 @@
 package bzz.it.uno.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -9,4 +15,30 @@ public class Message {
 	private int id;
 	private String text;
 	private User_Lobby userLobby;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	@Lob 
+	@Column(name="text", length=512)
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	@ManyToOne(targetEntity = User_Lobby.class)
+	public User_Lobby getUserLobby() {
+		return userLobby;
+	}
+	public void setUserLobby(User_Lobby userLobby) {
+		this.userLobby = userLobby;
+	}
+	
 }
