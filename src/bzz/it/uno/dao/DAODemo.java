a246f5e06c2ea5b5d231ea3ae52e76ccb9e5d6a3
SeverinHersche <35527435+SeverinHersche@users.noreply.github.com>
2019-06-23 21:21:30 +0200

    implemented the getAllUsers

--- src/bzz/it/uno/dao/DAODemo.java
+++
@@ -1,5 +0,0 @@
-package bzz.it.uno.dao;
-//Diese Klasse wird nicht gebrauch. Sie ist nur da, um die Ordnerstruktur auf Github zu puschen
-public class DAODemo {
-
-}
---
+++ src/bzz/it/uno/dao/UserDao.java
@@ -0,0 +1,12 @@
+package bzz.it.uno.dao;
+
+import java.util.List;
+
+import bzz.it.uno.model.User;
+
+//Diese Klasse wird nicht gebrauch. Sie ist nur da, um die Ordnerstruktur auf Github zu puschen
+public class UserDao {
+	public List<User> getAllUsers(){
+		return null;
+	}
+}
