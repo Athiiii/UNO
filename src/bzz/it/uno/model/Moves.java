d4777ae444e7694dab4732742a9a7909220bf252
SeverinHersche <35527435+SeverinHersche@users.noreply.github.com>
2019-06-25 08:06:10 +0200

    Done the moves Class

--- src/bzz/it/uno/model/Moves.java
+++ src/bzz/it/uno/model/Moves.java
@@ -1,6 +1,11 @@
 package bzz.it.uno.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -9,4 +14,28 @@ public class Moves {
 	private int id;
 	private User_Lobby user_Lobby;
 	private String playedCard;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	@ManyToOne(targetEntity = User_Lobby.class)
+	public User_Lobby getUser_Lobby() {
+		return user_Lobby;
+	}
+	public void setUser_Lobby(User_Lobby user_Lobby) {
+		this.user_Lobby = user_Lobby;
+	}
+	@Column(name = "playedCard")
+	public String getPlayedCard() {
+		return playedCard;
+	}
+	public void setPlayedCard(String playedCard) {
+		this.playedCard = playedCard;
+	}
+	
 }
