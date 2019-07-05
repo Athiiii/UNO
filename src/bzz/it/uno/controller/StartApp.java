f2fbee01a28c184d7b6b15658118caac0c84b11a
SeverinHersche <35527435+SeverinHersche@users.noreply.github.com>
2019-07-05 13:43:49 +0200

    added class startapp

---
+++ src/bzz/it/uno/controller/StartApp.java
@@ -0,0 +1,28 @@
+package bzz.it.uno.controller;
+
+import java.awt.EventQueue;
+
+import bzz.it.uno.dao.HandleConnectionToDB;
+
+public class StartApp {
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					HandleConnectionToDB.openDbFactory();
+					new LoginController();
+					Runtime.getRuntime().addShutdownHook(new Thread() {
+						//call before application will be closed
+						@Override
+						public void run() {
+							HandleConnectionToDB.closeDbFactory();
+						}
+					});
+				} catch (Exception e) {
+					e.printStackTrace();
+					HandleConnectionToDB.closeDbFactory();
+				}
+			}
+		});
+	}
+}
